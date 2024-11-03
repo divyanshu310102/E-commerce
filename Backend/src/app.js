758ce70a97925597cd75837e6b1e4d444d852bc8
divyanshu310102 <yadavdivyanshu489@gmail.com>
2024-11-03 22:46:24 +0530

    updated

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -20,7 +20,13 @@ app.use(cors({
     origin: `${process.env.CORS_ORIGIN}`,
     credentials: true,
     methods: 'GET,POST,PUT,DELETE',
-    allowedHeaders: 'Content-Type,Authorization',
+    allowedHeaders: [
+        "Content-Type",
+        "Authorization",
+        "Cache-Control",
+        "Expires",
+        "Pragma",
+      ],
 
 }))
 
